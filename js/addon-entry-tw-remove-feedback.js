f9650a2106d7d160bdb936cb900255ffc1f06f45
靳灿奇 <daylog@qq.com>
2026-04-18 03:59:31 +0800

    [skip ci] Deploy AI panel fix + tests

---
+++ js/addon-entry-tw-remove-feedback.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpGUI=window.webpackJsonpGUI||[]).push([[60],{1524:function(n,e,s){"use strict";s.r(e),s.d(e,"resources",(function(){return o}));var c=s(1525);const o={"remove-feedback.css":s.n(c).a}},1525:function(n,e,s){(n.exports=s(9)(!1)).push([n.i,'a[class^="menu-bar_feedback-link_"] {\n  display: none;\n}\n',""])}}]);
+//# sourceMappingURL=addon-entry-tw-remove-feedback.js.map
